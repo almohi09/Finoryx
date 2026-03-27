2d5d4ae384ee3348138039aafd888680d88055ba
almohi09 <mohdalmohi94@gmail.com>
2026-03-27 18:48:23 +0530

    fix: use dynamic PORT for render

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,13 @@ dns.setServers(['1.1.1.1']);
 const app = require('./src/app.js');
 const connectDb = require('./src/config/db');
 const dotEnv = require('dotenv');
+const PORT = process.env.PORT || 3000;
 
 dotEnv.config();
 connectDb();
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000");
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
 });
 
 
