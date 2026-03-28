acb83c5cebe113c94d381acf7856439d05af2300
almohi09 <mohdalmohi94@gmail.com>
2026-03-28 16:25:52 +0530

    fix: removed hash browser

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,22 @@ dotEnv.config();
 
 const app = require('./src/app.js');
 const connectDb = require('./src/config/db');
+
 const PORT = process.env.PORT || 3000;
 
-connectDb();
+async function startServer() {
+  try {
+    await connectDb();
+    console.log("DB connected");
 
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+    });
 
+  } catch (error) {
+    console.error("Failed to connect DB:", error);
+    process.exit(1);
+  }
+}
 
-
-
-
-
+startServer();
